729d0a21c071828d13e896164056e5132563ce34
Angus Russell <a5.russell@uwaterloo.ca>
2022-08-11 21:15:28 -0400

    Dependencies and testing
    Added package dependencies and a test set

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,14 @@
 using SelfEnergyAnalysis
 using Test
 
+
+
 @testset "SelfEnergyAnalysis.jl" begin
     # Write your tests here.
+    a = [1, NaN, 2, NaN, 3]
+    SelfEnergyAnalysis.remove_NaN!(a)
+    @test any(isnan.(a)) == false
+    b = [ NaN, NaN, NaN, NaN]
+    SelfEnergyAnalysis.remove_NaN!(b)
+    @test any(isnan.(b)) == false
 end
